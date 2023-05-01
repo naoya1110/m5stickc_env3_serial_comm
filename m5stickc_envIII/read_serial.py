e3d1bc0d7c912f010e98f8e2f0233ce4379d3120
naoya1110 <naoya.1110@gmail.com>
2023-05-01 11:52:10 +0900

    updated

--- m5stickc_envIII/read_serial.py
+++ m5stickc_envIII/read_serial.py
@@ -1,6 +1,9 @@
 import serial
 
-ser = serial.Serial('COM3',115200,timeout=3)
+
+#ser = serial.Serial('COM3',115200,timeout=3) # windows
+ser = serial.Serial('/dev/ttyUSB1',115200,timeout=3) # linux
+
 # clear buffer
 ser.reset_input_buffer()
 
